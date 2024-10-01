9a5e327c56a2f7b313803910952ac635e7002da4
Rokr0k <yangeh2225@gmail.com>
2024-10-01 15:48:07 +0900

    feat(board): add more features for board
    - Add board pivot clamp according to the specification and make it
      customizable just in case.
    - Edit `BoardControllerEditor` to customize pivot clamp.
    - Add support for board pivot clamp display.
    - Use `SpriteRenderer.size` and `BoxCollider2D.size` instead of
      `Transform.localScale` inside SetBoard function for more asthetical
      consistency.
    - Make board handle respond to board changes in edit mode.

--- Assets/Scripts/Board/BoardController.cs
+++ Assets/Scripts/Board/BoardController.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 
 namespace DontFall.Board
 {
+    [RequireComponent(typeof(HingeJoint2D), typeof(BoxCollider2D), typeof(SpriteRenderer))]
     public class BoardController : MonoBehaviour
     {
-        [SerializeField, Range(-0.5f, 0.5f)] private float pivot;
-        [SerializeField] private float width;
+        [SerializeField] private float pivot = 0;
+        [SerializeField] private Vector2 clamp = new Vector2(0.1f, 0.9f);
 
-        [SerializeField] private Vector2 edge1, edge2;
+        [SerializeField] private Vector2 edge1 = new Vector2(-1, 0), edge2 = new Vector2(1, 0);
+        
+        [SerializeField] private GameObject clampObject;
 
         public float Pivot
         {
             get => pivot;
             set
             {
-                pivot = Mathf.Clamp(value, -0.5f, 0.5f);
+                pivot = Mathf.Clamp(value, clamp.x - 0.5f, clamp.y - 0.5f);
                 
                 var joint = GetComponent<HingeJoint2D>();
                 joint.anchor = pivot * Vector2.right;
@@ -24,18 +27,31 @@ namespace DontFall.Board
 
         public Vector2 Edge1 => edge1;
         public Vector2 Edge2 => edge2;
+        public Vector2 Clamp => clamp;
 
         public void SetBoard()
         {
+            var renderer = GetComponent<SpriteRenderer>();
+            var collider = GetComponent<BoxCollider2D>();
+
             transform.position = Vector2.Lerp(edge1, edge2, 0.5f);
-            transform.localScale = new Vector3(Vector2.Distance(edge1, edge2), width, 1);
             transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(edge2.y - edge1.y, edge2.x - edge1.x) * Mathf.Rad2Deg, Vector3.forward);
 
-            var rigid = GetComponent<Rigidbody2D>();
-            rigid.velocity = Vector2.zero;
-            rigid.angularVelocity = 0;
+            renderer.size = collider.size = new Vector2((edge2 - edge1).magnitude, renderer.size.y);
 
             Pivot = Pivot;
+
+            if (clampObject != null)
+            {
+                var clampRenderer = clampObject.GetComponent<SpriteRenderer>();
+
+                Vector2 clampedEdge1 = Vector2.Lerp(edge1, edge2, clamp.x), clampedEdge2 = Vector2.Lerp(edge1, edge2, clamp.y);
+
+                clampObject.transform.position = Vector2.Lerp(clampedEdge1, clampedEdge2, 0.5f);
+                clampObject.transform.localRotation = Quaternion.identity;
+
+                clampRenderer.size = new Vector2((clampedEdge2 - clampedEdge1).magnitude, clampRenderer.size.y);
+            }
         }
 
         void OnDrawGizmosSelected()
@@ -43,6 +59,10 @@ namespace DontFall.Board
             Gizmos.color = Color.red;
             Gizmos.DrawLine(edge1, edge2);
             Gizmos.DrawWireSphere(Vector2.Lerp(edge1, edge2, pivot + 0.5f), 0.1f);
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(Vector2.Lerp(edge1, edge2, clamp.x), 0.1f);
+            Gizmos.DrawWireSphere(Vector2.Lerp(edge1, edge2, clamp.y), 0.1f);
         }
     }
 }
--- Assets/Scripts/Board/HandleController.cs
+++ Assets/Scripts/Board/HandleController.cs
@@ -4,23 +4,29 @@ using UnityEngine.EventSystems;
 
 namespace DontFall.Board
 {
+    [ExecuteInEditMode]
     public class HandleController : MonoBehaviour, IDragHandler
     {
         [SerializeField] private BoardController board;
 
-        private void Start()
+        private void Update()
         {
-            transform.position = Vector2.Lerp(board.Edge1, board.Edge2, board.Pivot + 0.5f);
+            if (board != null)
+            {
+                transform.position = Vector2.Lerp(board.Edge1, board.Edge2, board.Pivot + 0.5f);
+            }
         }
 
         void IDragHandler.OnDrag(PointerEventData pointer)
         {
-            Vector2 pos = Camera.main.ScreenToWorldPoint(pointer.position);
+            if (board != null)
+            {
+                Vector2 pos = Camera.main.ScreenToWorldPoint(pointer.position);
 
-            var pivotValue = Vector2.Dot(pos - board.Edge1, (board.Edge2 - board.Edge1).normalized) / (board.Edge2 - board.Edge1).magnitude;
+                var pivotValue = Vector2.Dot(pos - board.Edge1, (board.Edge2 - board.Edge1).normalized) / (board.Edge2 - board.Edge1).magnitude;
 
-            transform.position = Vector2.Lerp(board.Edge1, board.Edge2, pivotValue);
-            board.Pivot = pivotValue - 0.5f;
+                board.Pivot = pivotValue - 0.5f;
+            }
         }
     }
 }
