f2371a532c550225a17c023b830dea8b5293fa27
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2024-11-01 17:45:50 +0900

    권동현 - 게임매니저 Drag&Drop

---
+++ Assets/Scripts/DragUI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class DragUI : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public int index = 0;
+    private CanvasGroup canvasGroup;
+    private GameObject currentDraggedObject;
+
+    private void Awake()
+    {
+        canvasGroup = gameObject.GetComponentInParent<CanvasGroup>();
+    }
+
+    //드래그 시작 시 호출
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //부모 오브젝트는 인벤토리의 빈칸이 됨
+        GameObject go = gameObject.transform.parent.gameObject;
+        GameManager.Instance.EmptyInventory.Add(go);
+        canvasGroup.blocksRaycasts = false;
+
+        //이미지는 끄고 정해진 오브젝트를 생성
+        gameObject.GetComponent<Image>().enabled = false;
+        currentDraggedObject = Instantiate(GameManager.Instance.GamePrefab[index].gameObj);
+        currentDraggedObject.GetComponent<DragObj>().index = index;
+    }
+
+    //드래그 중 계속 호출
+    public void OnDrag(PointerEventData eventData)
+    {
+        // 드래그한 오브젝트가 마우스를 따라 움직이게 설정
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        currentDraggedObject.transform.position = mousePos;
+    }
+
+    //드래그 끝날 때 호출
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Destroy(gameObject);
+        canvasGroup.blocksRaycasts = true;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,29 +6,36 @@ using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+[Serializable]
+public class ObjectInfo
 {
-    private int Count;
-    private int Score;
-    private bool IsOver;
+    public GameObject gameObj;
+    public GameObject gameUI;
+}
+
+//게임의 시작, 실패, 초기화 관리
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    private int Point; //초기에 주어지는 포인
+    private int Score; //선반 위에 올려진 물체에 매겨진 점수 합산
+    private bool IsOver; //게임 오버 제어
 
     private Object lastObject;
-    [SerializeField] private GameObject[] ObjectPrefab;
-    [SerializeField] private GameObject effectPrefab;
-    [SerializeField] private Transform ObjectGroup;
+    [SerializeField] private GameObject ObjectGroup;
+    [SerializeField] List<ObjectInfo> gamePrefab;
+    [SerializeField] Canvas canvas;
 
-    [Range(1, 30)]
-    [SerializeField] private int PoolSize;
-    [SerializeField] private int PoolCursor;
-
-    private GameObject[] HiddenObj;
-
-    //싱글톤
-    private static GameManager instance;
     public static GameManager Instance => instance;
+    public List<ObjectInfo> GamePrefab => gamePrefab;
+    public Canvas Canvas => canvas;
+    public List<GameObject> EmptyInventory;
+
 
     private void Awake()
     {
+        instance = this;
         Application.targetFrameRate = 60; //프레임 60으로 고정
     }
 
@@ -38,61 +46,7 @@ public class GameManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
     public void GameStart()
     {
-        Invoke(nameof(NextObject), 1.5f);
-    }
 
-    Object MakeObject()
-    {
-        int index = Random.Range(0, ObjectPrefab.Length);
-
-        //새로운 오브젝트를 오브젝트 그룹에 상속하여 생성
-        GameObject instantObject = Instantiate(ObjectPrefab[index], ObjectGroup);
-        instantObject.name = "Object" + ++Count;
-        Object instantObj = instantObject.GetComponent<Object>();
-
-        return instantObj;
-    }
-
-    void NextObject()
-    {
-        if (IsOver)
-        {
-            return;
-        }
-
-        lastObject = MakeObject();
-
-        StartCoroutine(WaitNext());
-    }
-
-    //코루틴 생성
-    IEnumerator WaitNext()
-    {
-        yield return new WaitWhile(() => lastObject != null);
-        //2.5초 휴식
-        yield return new WaitForSeconds(2.5f);
-
-        NextObject();
-    }
-
-    void IPointerDownHandler.OnPointerDown(PointerEventData data)
-    {
-        if (lastObject == null)
-        {
-            return;
-        }
-
-        lastObject.Drag();
-    }
-    void IPointerUpHandler.OnPointerUp(PointerEventData data)
-    {
-        if (lastObject == null)
-        {
-            return;
-        }
-
-        lastObject.Drop();
-        lastObject = null;
     }
 
     public void GameOver()
@@ -105,30 +59,32 @@ public class GameManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
         StartCoroutine(GameOverRoutine());
     }
+
     IEnumerator GameOverRoutine()
     {
         //활성화 된 모든 오브젝트 가져오기
         Object[] objs = FindObjectsOfType<Object>();
 
-        //모든 오브젝트의 물리효과 제거
-        foreach(var obj in objs)
-        {
-            obj.Rb2d.simulated = false;
-        }
+        ////모든 오브젝트의 물리효과 제거
+        //foreach(var obj in objs)
+        //{
+        //    obj.Rb2d.simulated = false;
+        //}
 
         //모든 오브젝트에 접근해서 하나씩 지우기
         foreach(var obj in objs)
         {
-            obj.GetComponent<Animator>().SetTrigger("Destroy");
+            //obj.GetComponent<Animator>().SetTrigger("Destroy");
+            Destroy(obj);
             yield return new WaitForSeconds(0.1f);
         }
 
         yield return new WaitForSeconds(1f);
 
         //게임오버 씬으로 이동
-
     }
 
+    //초기화 버튼 이벤트
     public void Restart()
     {
         StartCoroutine(ResetCorutine());
