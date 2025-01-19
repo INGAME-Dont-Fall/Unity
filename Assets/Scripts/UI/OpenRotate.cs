db030d26840a1acdedb2f4d4c2ab058068f4ddc5
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2025-01-19 22:26:01 +0900

    UI
    버튼돌리기

---
+++ Assets/Scripts/UI/OpenRotate.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class OpenRotate : MonoBehaviour
+{
+    [SerializeField] private RectTransform imageTransform;
+
+    public void RotateImage()
+    {
+        imageTransform.Rotate(0,0,180);
+    }
+
+}
--- Assets/Scripts/UI/ScoreUI.cs
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
 using TMPro;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
+using DontFall.Transition;
+
 public class ScoreUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text textTotalScore;
     [SerializeField] private TMP_Text textRoundScore;
     [SerializeField] private ScoreSO scoreData;
+    [SerializeField] TransitionManager transitionManager;
 
     void Start()
     {
         textTotalScore.text = "" + scoreData.totalScore;
         textRoundScore.text = "" + scoreData.roundScore;
+    }
+
+    public void LoadPlayScene()
+    {
+        transitionManager.StartTransition(true, true, ()=>
+        {
+            SceneManager.LoadScene("PlayScene");
+        });
     }
 }
