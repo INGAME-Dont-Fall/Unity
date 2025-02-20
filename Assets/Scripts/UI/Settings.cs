9717cde24a1ace3a4a1db005dc783ecc1e4e282e
Rokr0k <rokr0k@rokr0k.xyz>
2025-02-20 19:33:10 +0900

    display basic settings dialog without functionalities

--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -4,14 +4,21 @@ namespace DontFall.UI
 {
     public class Settings : MonoBehaviour
     {
+        private void Awake()
+        {
+            CloseOption();
+        }
+
         public void OpenOption()
         {
             Time.timeScale = 0;
+            gameObject.SetActive(true);
         }
 
         public void CloseOption()
         {
             Time.timeScale = 1;
+            gameObject.SetActive(false);
         }
     }
 }
